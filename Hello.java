40fafcca201bb0fa3b64449140268dbf3aaa9ae8
LittleBigCheese <69914400+LittleBigCheese@users.noreply.github.com>
2020-08-19 12:03:24 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/***********************************************
+*Hello.java
+************************************************
+*Benji Ashwell
+*8/19/20
+*CMSC 255( I am unsure of my section)
+************************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Benji Ashwell");
+		System.out.println("Hello.java");
+		System.out.println("Test to see if Java works on my personal computer");
+		System.out.println("8/19/20");
+		System.out.println("CMSC 255");
+		System.out.println();
+		}
+	}
+		
